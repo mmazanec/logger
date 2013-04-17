b325e2a409101da78febde95e5589ed50e00a019
mmazanec <github@tycho.otherinbox.com>
2013-04-17 10:35:32 -0600

    Update logger.php
    Changed license to MIT

--- logger.php
+++ logger.php
@@ -2,18 +2,30 @@
 /**
  * Part of the Logger bundle for Laravel v3
  *
- * NOTICE OF LICENSE
+ * The MIT License (MIT)
+ * Copyright (c) 2013 Mike Mazanec <https://github.com/mmazanec>
  *
- * Licensed under the 3-clause BSD License.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in the
+ * Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, subject to the 
+ * following conditions:
  *
- * This source file is subject to the 3-clause BSD License that is
- * bundled with this package in the LICENSE file.  It is also available at
- * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ * The above copyright notice and this permission notice shall be included in all copies 
+ * or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * @package    Logger
  * @version    1.0
  * @author     Mike Mazanec
- * @license    BSD License (3-clause)
+ * @license    MIT
  * @copyright  (c) 2013 Mike Mazanec
  */
 
